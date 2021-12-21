a60eea439a2d5107cff9bdaf22e80d96963dfc51
Diaz Daniel Maximiliano <50111435+MaxiDiaz11@users.noreply.github.com>
2021-12-20 22:08:12 -0300

    Inicio de proyecto, Header incluido

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;

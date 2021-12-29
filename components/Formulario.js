ae0e16bc19cc4ca541b163c6b2ee07989eee805a
Diaz Daniel Maximiliano <50111435+MaxiDiaz11@users.noreply.github.com>
2021-12-29 20:13:34 -0300

    Se agrego la API de cryptomonedas

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,13 +1,31 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
+import axios from 'axios';
 
 const Formulario = () => {
+  const [moneda, setMoneda] = useState('');
+  const [criptomoneda, setCriptomoneda] = useState('');
+  const [criptomonedas, setCriptomonedas] = useState('');
+
+  useEffect(() => {
+    const getCryptos = async () => {
+      const response = await axios.get(
+        'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD',
+      );
+      setCriptomonedas(response.data.Data);
+    };
+    getCryptos();
+  }, []);
+
   return (
     <View>
       <Text style={styles.label}>Moneda</Text>
-      <Picker>
+      <Picker
+        onValueChange={moneda => setMoneda(moneda)}
+        selectedValue={moneda}
+        itemStyle={{height: 120}}>
         <Picker.Item label="--Seleccione--" value="" />
         <Picker.Item label="Dolar" value="USD" />
         <Picker.Item label="Peso Argentino" value="ARS" />
@@ -15,6 +33,19 @@ const Formulario = () => {
         <Picker.Item label="Libra Esterlina" value="GBP" />
       </Picker>
       <Text style={styles.label}>Criptomoneda</Text>
+      <Picker
+        onValueChange={crypto => setCriptomoneda(crypto)}
+        selectedValue={criptomoneda}
+        itemStyle={{height: 120}}>
+        <Picker.Item label="--Seleccione--" value="" />
+        {criptomonedas.map(c => (
+          <Picker.Item
+            key={c.CoinInfo.Id}
+            label={c.CoinInfo.FullName}
+            value={c.CoinInfo.Name}
+          />
+        ))}
+      </Picker>
     </View>
   );
 };
